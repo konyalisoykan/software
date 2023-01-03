a256d3004c702c5b451427edac57f8c5f82ff153
Soykan Konyalı <konyalisoykan@gmail.com>
2023-01-03 17:40:12 +0300

    AbstractFactory eklendi

---
+++ BackendApps/BackendApps/DesignPaterns/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApps.DesignPaterns.AbstractFactory
+{
+    public abstract class AbstractFactory
+    {
+        public abstract void Log(string Message);
+    }
+    public abstract class Cashibg
+    {
+        public abstract void Cash(string Message);
+    }
+    public abstract class CrossCuttingConsernsFactory
+    {
+        public abstract AbstractFactory CreateLogger();
+        public abstract Cashibg CreateCashing();
+    }
+    public class Factory1: CrossCuttingConsernsFactory
+    {
+        public override AbstractFactory CreateLogger() 
+        { 
+        return  new  Log4net();
+        }
+        public override Cashibg CreateCashing()
+        {
+            return new RedisCash();
+        }
+    }
+    public class Factory2 : CrossCuttingConsernsFactory
+    {
+        public override AbstractFactory CreateLogger()
+        {
+            return new Nlogger();
+        }
+        public override Cashibg CreateCashing()
+        {
+            return new RedisCash();
+        }
+        public class Factory3 : CrossCuttingConsernsFactory
+        {
+            public override AbstractFactory CreateLogger()
+            {
+                return new Nlogger();
+            }
+            public override Cashibg CreateCashing()
+            {
+                return new MemeCash();
+            }
+        }
+    }
+    
+    public class ProductManager
+    {
+        private AbstractFactory _logging;
+        private Cashibg _cashing;
+        CrossCuttingConsernsFactory _crossCuttingConsernsFactory;
+        public ProductManager(CrossCuttingConsernsFactory crossCuttingConsernsFactory)
+        {
+            _logging = crossCuttingConsernsFactory.CreateLogger();
+            _cashing = crossCuttingConsernsFactory.CreateCashing();
+            _crossCuttingConsernsFactory = crossCuttingConsernsFactory;
+
+        }
+        public void  GetAll()
+        {
+            _logging.Log("Loglandı");
+            _cashing.Cash("Casjlendi");
+            Console.WriteLine("Ürünler listelendi");
+        }
+
+    }
+       
+    public class Log4net : AbstractFactory
+    {
+        public override void Log(string Message)
+        {
+           Console.WriteLine("Log4net");  
+        }
+    }
+    public class Nlogger : AbstractFactory
+    {
+        public override void Log(string Message)
+        {
+            Console.WriteLine("Nkogger");
+        }
+    }
+    public class MemeCash : Cashibg
+    {
+        public override void Cash(string Message)
+        {
+            Console.WriteLine("MemeCash");
+        }
+    }
+    public class RedisCash : Cashibg
+    {
+        public override void Cash(string Message)
+        {
+            Console.WriteLine("Redşs");
+        }
+    }
+}
--- BackendApps/BackendApps/Program.cs
+++ BackendApps/BackendApps/Program.cs
@@ -1,5 +1,7 @@
-﻿using BackendApps.DesignPaterns.Factory;
+﻿using BackendApps.DesignPaterns.AbstractFactory;
+using BackendApps.DesignPaterns.Factory;
 using BackendApps.DesignPaterns.Singleton;
+using static BackendApps.DesignPaterns.AbstractFactory.Factory2;
 
 namespace BackendApps
 {
@@ -11,6 +13,8 @@ namespace BackendApps
             Console.WriteLine("Singlaton!");
             DesignPaterns.Factory.CustomerManager _factoryCustomerManager = new DesignPaterns.Factory.CustomerManager(new LoggerFactoy());
             _factoryCustomerManager.Save();
+            ProductManager _productManager = new ProductManager(new Factory2());
+            _productManager.GetAll();
             Console.ReadLine();
         }
     }
