cb82b2b43b252c1a9a1121938704bb832e7881a0
Soykan Konyalı <konyalisoykan@gmail.com>
2023-01-03 19:00:28 +0300

    Prototıp design eklendi

---
+++ BackendApps/BackendApps/DesignPaterns/Prototype/Prototype.cs
@@ -0,0 +1,36 @@
+﻿using BackendApps.DesignPaterns.AbstractFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApps.DesignPaterns.Prototype
+{
+   
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public string? FirstName { get; set; }
+        public  string LastName { get; set; }
+
+    }
+    public class Customer: Person
+    {
+        public string  City { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+
+    }
+    public class Employee : Person
+    {
+        public string Salary { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+
+    }
+}
--- BackendApps/BackendApps/Program.cs
+++ BackendApps/BackendApps/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BackendApps.DesignPaterns.AbstractFactory;
 using BackendApps.DesignPaterns.Factory;
+using BackendApps.DesignPaterns.Prototype;
 using BackendApps.DesignPaterns.Singleton;
 using static BackendApps.DesignPaterns.AbstractFactory.Factory2;
 
@@ -15,6 +16,11 @@ namespace BackendApps
             _factoryCustomerManager.Save();
             ProductManager _productManager = new ProductManager(new Factory2());
             _productManager.GetAll();
+            Customer customer1 = new Customer { FirstName = "Ali", LastName = "Veli", City = "ist" };
+            Customer customer2 =(Customer) customer1.Clone();
+            customer2.FirstName = "Soykan";
+            Console.WriteLine("Customer1 fitsyname {0}", customer1.FirstName);
+            Console.WriteLine("Customer2 fitsyname {0}", customer2.FirstName);
             Console.ReadLine();
         }
     }
